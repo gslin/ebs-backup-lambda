c6e4d6b43457f74e99268c160a3481d197e48013
Gea-Suan Lin <gslin@gslin.org>
2018-07-17 08:39:34 +0800

    Add ebs-backup.py.

---
+++ ebs-backup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import boto3
+import json
+import time
+
+def lambda_handler(event, context):
+    ec2 = boto3.resource('ec2')
+    volumes = ec2.volumes.filter(Filters=[{'Name': 'tag:DailyBackup', 'Values': ['1']}])
+
+    for volume in volumes:
+        for tag in volume.tags:
+            if 'Name' == tag['Key']:
+                name = tag['Value']
+
+        if name is None:
+            continue
+
+        now = time.strftime('%Y%m%d-%H%M%S')
+
+        desc = 'Snapshoted from %s (%s, %s)' % (name, volume.id, now)
+        snapshot = volume.create_snapshot(Description=desc)
+
+        snap_name = '%s-snapshot-%s' % (name, now)
+
+        snapshot.create_tags(Tags=[{
+            'Key': 'Name',
+            'Value': snap_name
+        }])
